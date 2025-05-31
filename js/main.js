bf80f18212643d1ca8cec3418f01eaf87a7c52fc
MamadD78 <mamad.d.2000.4010@gmail.com>
2025-05-31 17:25:50 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+let remainigTime = 70000;
+
+function setTime(){
+    if(remainigTime == 0) return;
+    let h = Math.floor(remainigTime/3600);
+    let m = Math.floor((remainigTime%3600)/60);
+    let s = Math.floor(remainigTime%3600)%60;
+    document.querySelector('#hours').innerHTML = h
+    document.querySelector('#minutes').innerHTML = m
+    document.querySelector('#seconds').innerHTML = s
+}
+
+setInterval(()=>{
+    remainigTime -= 1;
+    setTime()
+}, 1000)
+
+
+function setSlide(input,index){
+    slideIndex = index;
+    let item = document.querySelector(`#${input}`)
+    let slides = [...document.querySelector('.slides').children];
+    slides.forEach((element)=> {
+        element.classList.remove('active');
+    })
+    item.classList.add('active');
+}
+
+setInterval(() => {
+    slideIndex += 1;
+    if(slideIndex == 4){
+        slideIndex = 1;
+    }
+    setSlide(`slide${slideIndex}` , slideIndex)
+}, 5000);
